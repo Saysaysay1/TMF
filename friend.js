fa8f0ce2e0b9191b96a42dd4d0b220037c1a5831
Saysaysay1 <126253480+Saysaysay1@users.noreply.github.com>
2024-04-29 16:34:01 +0800

    friend.js

--- friend.js
+++ friend.js
@@ -1,2 +1,6 @@
-alert('Hi')
-alert('Hello')
+        alert('Welcome to our Friendship Day celebration!');
+        alert('We appreciate your friendship and support.');
+        alert('Friendship is a treasure that we cherish.');
+        alert('Thank you for being a part of our journey.');
+        alert('May our bond of friendship grow stronger every day.');
+        alert('Please read my heartfelt message, you can read it by clicking the Image.')
